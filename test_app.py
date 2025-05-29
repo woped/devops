86192fc03c2e1543482f41dca3e2934751dc71fb
I569996 <maximilian.otto@sap.com>
2025-05-29 11:47:45 +0200

    Red

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import average_age
 
 def test_average_age():
     data = [{"name": "Alice", "age": 30}, {"name": "Bob", "age": 40}]
-    assert average_age(data) == 35
+    assert average_age(data) == 39
