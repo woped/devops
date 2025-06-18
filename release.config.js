66083c74d62d3985b21782272be1d6fba77a8ee9
Moritz010902 <moritz.neumann02@gmail.com>
2025-06-18 22:38:15 +0200

    fix: mismatch github url in woped-web

--- release.config.js
+++ release.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // always use the repository that is currently cloned
+  repositoryUrl: `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}.git`,
   plugins: [
     [
       "@semantic-release/commit-analyzer",
